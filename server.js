042a7e2deeb089ecfe927bb4d6e38f35a4f2f891
edean78 <edean78@gmail.com>
2020-01-13 23:20:17 -0500

    added route for saving and updating headline associated comment

--- server.js
+++ server.js
@@ -86,6 +86,25 @@ app.get("/headline/:id", function(req, res){
     });
 });
 
+// Route for saving/updating Headline's associated Comment
+app.post("/headline/:id", function(req, res){
+    // Create a new comment and pass the req.body to the entry
+    db.Comment.create(req.body).then(function(dbComment){
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Headline.findOneAndUpdate({id: req.params.id}, {comment: dbComment._id}, {new:true});  
+    })
+    .then(function(dbHeadline){
+        // If we were able to successfully update an Headline, send it back to the client
+         res.json(dbHeadline);
+    })
+    .catch(function(err){
+        // If an error occured, send it to the client
+        res.json(err);
+    });
+});
+
 // Start the server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
